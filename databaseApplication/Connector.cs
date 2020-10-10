df186dcc6319496e122f0ba7fa9d01543d696a58
tlasmanis <lasmanit@cu.coventry.ac.uk>
2020-10-10 23:34:43 +0100

    added public methods

--- databaseApplication/Connector.cs
+++ databaseApplication/Connector.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,10 +15,40 @@ namespace databaseApplication
         private string userID;
         private string password;
         private string connectionString;
+
         private SqlConnection cnn;
         private SqlCommand cmd;
         private SqlDataReader dR;
         private SqlDataAdapter dA;
         private SqlCommandBuilder cB;
+
+        public bool connTest()
+        {
+            cnn = new SqlConnection(connectionString);
+        }
+
+        public bool createName(string name)
+        {
+
+        }
+
+        public bool createNamePostcode(string name, string postcode)
+        {
+
+        }
+
+        public string getID(string name)
+        {
+
+        }
+
+        public DataSet readAll()
+        {
+
+        }
+
+
+
+
     }
 }
