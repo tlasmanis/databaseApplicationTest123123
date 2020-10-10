28658fd7f373bd9f2e628a5025f7fc76960364e9
tlasmanis <lasmanit@cu.coventry.ac.uk>
2020-10-10 22:38:49 +0100

    added methods
    added methods

--- databaseApplication/Connector.cs
+++ databaseApplication/Connector.cs
@@ -8,6 +8,15 @@ namespace databaseApplication
 {
     class Connector
     {
-
+        private string dataSource;
+        private string initialCatalog;
+        private string userID;
+        private string password;
+        private string connectionString;
+        private SqlConnection cnn;
+        private SqlCommand cmd;
+        private SqlDataReader dR;
+        private SqlDataAdapter dA;
+        private SqlCommandBuilder cB;
     }
 }
